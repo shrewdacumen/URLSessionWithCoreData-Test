25cd3d5fb1e5d7190bf80da2e25f7dfe29592560
Sungwook Kim <discerningacumen@gmail.com>
2021-06-14 16:42:29 +0900

    Initial Commit

---
+++ URLSession Test/URLSession_TestApp.swift
@@ -0,0 +1,20 @@
+//
+//  URLSession_TestApp.swift
+//  URLSession Test
+//
+//  Created by sungwook on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_TestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
